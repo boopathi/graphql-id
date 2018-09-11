d8fa4a15984be8792518a4e7e296c4eb6377534b
Boopathi Rajaa <me@boopathi.in>
2018-09-11 13:33:37 +0200

    fix: compile errors for tests

--- tests/select_operation_test.rs
+++ tests/select_operation_test.rs
@@ -5,51 +5,53 @@ use graphql_parser::parse_query;
 use graphql_select::select_operation;
 
 enum QueryStr {
-  Actual(String),
-  Expected(&'static str),
+    Actual(String),
+    Expected(&'static str),
 }
 
 fn normalize(query: QueryStr) -> String {
-  let query = match query {
-    QueryStr::Actual(query) => query.to_string(),
-    QueryStr::Expected(query) => query,
-  };
-  format!("{}", parse_query(query).unwrap())
+    let query = match query {
+        QueryStr::Actual(query) => query.to_string(),
+        QueryStr::Expected(query) => query.to_string(),
+    };
+    format!("{}", parse_query(query.as_ref()).unwrap())
 }
 
 #[test]
 fn single_operation_should_be_no_op() {
-  let query = "
-    query A {
-      foo
-    }
-  ";
+    let query = "
+        query A {
+          foo
+        }
+    ";
 
-  assert_eq!(
-    normalize(select_operation(&query, &"A").unwrap()),
-    normalize(query)
-  );
+    assert_eq!(
+        normalize(QueryStr::Actual(select_operation(&query, &"A").unwrap())),
+        normalize(QueryStr::Expected(query))
+    );
 }
 
 #[test]
 fn select_from_multiple_queries() {
-  let query = "
-    query A {
-      foo
-    }
-    query B {
-      bar
-    }
-  ";
-
-  assert_eq!(
-    normalize(select_operation(&query, &"A").unwrap()),
-    normalize(
-      "
+    let query = "
         query A {
           foo
         }
-      "
-    )
-  );
+        query B {
+          bar
+        }
+    ";
+
+    assert_eq!(
+        normalize(QueryStr::Actual(select_operation(&query, &"A").unwrap())),
+        normalize(
+            QueryStr::Expected(
+                "
+                    query A {
+                      foo
+                    }
+                "
+            )
+        )
+    );
 }
--- tests/visitor_test.rs
+++ tests/visitor_test.rs
@@ -8,20 +8,21 @@ use graphql_select::visitor::*;
 
 #[test]
 fn name_visitor() {
-  struct NameVisitor;
-  impl Visitor for NameVisitor {
-    fn visit_query_enter(&mut self, query: &Query) -> Result<(), GraphQLError> {
-      assert_eq!(query.name.as_ref().unwrap(), "Foo");
-      Ok(())
+    struct NameVisitor;
+
+    impl Visitor for NameVisitor {
+        fn visit_query_enter(&mut self, query: &Query) -> Result<(), GraphQLError> {
+            assert_eq!(query.name.as_ref().unwrap(), "Foo");
+            Ok(())
+        }
     }
-  }
 
-  let document = parse_query("query Foo { foo }").unwrap();
-  let mut visitor = NameVisitor {};
+    let document = parse_query("query Foo { foo }").unwrap();
+    let mut visitor = NameVisitor {};
 
-  let mut traversal = Traversal {
-    visitor: &mut visitor,
-  };
+    let mut traversal = Traversal {
+        visitor: &mut visitor,
+    };
 
-  assert_eq!(traversal.handle_document(&document).is_ok(), true);
+    assert_eq!(traversal.handle_document(&document).is_ok(), true);
 }
